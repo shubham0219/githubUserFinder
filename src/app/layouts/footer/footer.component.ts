eebaccd98c965bb38fde5177071266fdcaa9ef0d
shubham0219 <65565631+shubham0219@users.noreply.github.com>
2021-05-26 18:05:04 +0530

    first commit

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+author: string ="Shubham Mankar";
   constructor() { }
 
   ngOnInit(): void {
